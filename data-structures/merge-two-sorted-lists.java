702d076aa586bffbe00acbc0ce64cb109867d588
Tracy Marshall <tracythedev@gmail.com>
2020-02-29 14:34:06 -0500

    added merge-two-sorted-lists

---
+++ data-structures/merge-two-sorted-lists.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        // Create a variable to store the current node being worked on as well as a
+        // Variable to point to the head node
+        ListNode dummy = new ListNode(-1);
+        ListNode head = dummy;
+        
+        // Loop through both lists to check which node has the least value. Push that node into
+        // the new list
+        while(l1 != null && l2 != null) {
+            if(l1.val < l2.val) {
+                dummy.next = l1;
+                // Replace the node of lesser value with its next property, moving 
+                // the list forward
+                l1 = l1.next;
+            } else {
+                dummy.next = l2;
+                l2 = l2.next;
+            }
+            // Set the dummy variable to the next value in its node, moving the variable forward
+            dummy = dummy.next;
+        }
+        
+        // Once one of the lists are empty, the loop will end and the rest of the other list will be
+        // appended to the new list
+        if(l1 != null) {
+            dummy.next = l1;
+        } else {
+            dummy.next = l2;
+        }
+        
+        // Return the next property of the head variable, which still has a reference to the original
+        // ListNode created when dummy was declared
+        return head.next;
+    }
+}
---
+++ string-manipulation/valid-parentheses.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isValid(String s) {
+        // Check if the string is even. All valid strings should be
+        if(s.length() % 2 != 0) {return false;}
+        
+        // Create a stack to check the combinations of parentheses
+        Stack<Character> stack = new Stack();
+        
+        // Loop through each character in the string to push and pop the stack if necessary
+        for(char x : s.toCharArray()) {
+            if(x == '(' || x == '{' || x == '[') {
+                // Push any opening bracket into the stack. Any closing bracket is compared to
+                // the last element in the stack to see if they match, provided the stack is
+                // not empty.
+                stack.push(x);
+            } else if(x == ')' && !stack.isEmpty() && stack.peek() == '(') {
+                stack.pop();
+            } else if(x == '}' && !stack.isEmpty() && stack.peek() == '{') {
+                stack.pop();
+            } else if(x == ']' && !stack.isEmpty() && stack.peek() == '[') {
+                stack.pop();
+            }
+        }
+        
+        // Return whether the stack is empty or not
+        return stack.isEmpty();
+    }
+}
