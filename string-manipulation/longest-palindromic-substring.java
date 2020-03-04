51b0a6efdc38abce0438d0e76a3e9438d281c250
Tracy Marshall <tracythedev@gmail.com>
2020-03-04 03:09:24 -0500

    added strategy to try for longest-palindromic-substring

---
+++ string-manipulation/longest-palindromic-substring.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String longestPalindrome(String s) {
+        // Reverse the string and store it into a variable
+        
+        // Initialize the temp variable and the variable to store the output variable
+        
+        // Loop through the input string and compare it against the reversed string
+            // If the character is equal to the other character, add it to the temp storage variable
+            
+            // If not, compare the temp variable to the output variable in terms of string size
+            // and replace strings if the temp variable is larger
+        
+        // Create a helper function that compares the string length in the temp variable to the string
+        // in the output variable. Replace the string in the output if the temp string is larger
+    }
+}
