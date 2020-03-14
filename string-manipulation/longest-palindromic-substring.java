fb5fe7c1562545f1d5648348c7f7e4f789f5b5e9
Tracy Marshall <tracythedev@gmail.com>
2020-03-14 02:12:00 -0400

    added solution for longest-palindromic-substring

--- string-manipulation/longest-palindromic-substring.java
+++ string-manipulation/longest-palindromic-substring.java
@@ -1,3 +1,38 @@
+// O(n^2) time complexity, O(1) space complexity
+class Solution {
+    public String longestPalindrome(String s) {
+        if(s == null || s.length() < 1) return "";
+        
+        int start = 0;
+        int end = 0;
+        
+        for(int i = 0; i < s.length(); i++) {
+            int len1 = palLength(s, i, i);
+            int len2 = palLength(s, i, i+1);
+            int len = Math.max(len1, len2);
+            
+            if(len > end - start + 1) {
+                start = i - ((len - 1) / 2);
+                end = i + (len / 2);
+            }
+        }
+        
+        return s.substring(start, end + 1);
+    }
+    
+    public int palLength(String s, int left, int right) {
+        if(s == null || left > right) return 0;
+        
+        while(left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+        
+        return right - left - 1;
+    }
+}
+
+// I want to test out this solution
 class Solution {
     public String longestPalindrome(String s) {
         // Reverse the string and store it into a variable
