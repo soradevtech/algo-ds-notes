e37bde1f00b40f58b3e4489152f5db56be59b2ec
Tracy Marshall <tracythedev@gmail.com>
2020-08-15 05:47:49 -0400

    Added problem descriptions

--- string-manipulation/longest-palindromic-substring.java
+++ string-manipulation/longest-palindromic-substring.java
@@ -1,3 +1,18 @@
+/**
+Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
+
+Example 1:
+
+Input: "babad"
+Output: "bab"
+Note: "aba" is also a valid answer.
+
+Example 2:
+
+Input: "cbbd"
+Output: "bb"
+*/
+
 // O(n^2) time complexity, O(1) space complexity
 class Solution {
     public String longestPalindrome(String s) {
--- string-manipulation/valid-parentheses.java
+++ string-manipulation/valid-parentheses.java
@@ -1,3 +1,14 @@
+/**
+Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+    Open brackets must be closed by the same type of brackets.
+    Open brackets must be closed in the correct order.
+
+Note that an empty string is also considered valid.
+*/
+
 class Solution {
     public boolean isValid(String s) {
         // Check if the string is even. All valid strings should be
